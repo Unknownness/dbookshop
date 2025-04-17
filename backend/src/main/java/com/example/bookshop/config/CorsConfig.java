8612aa428f6c57489de245eec882de0e692fc69b
Unknownness <165930985+Unknownness@users.noreply.github.com>
2025-04-17 23:30:45 +0300

    Update CorsConfig.java

--- backend/src/main/java/com/example/bookshop/config/CorsConfig.java
+++ backend/src/main/java/com/example/bookshop/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("https://frontendv2-oe6z.onrender.com")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true)
